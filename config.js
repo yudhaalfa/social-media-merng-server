fd8028f7ba4849bcd0ed1516c9fcc7de1e9cc64b
Yudha <yuualfar@gmail.com>
2021-02-09 23:59:21 +0700

    server merng

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb://user:EBCu8IVh3ZOIO0OS@cluster0-shard-00-00.ganlz.mongodb.net:27017,cluster0-shard-00-01.ganlz.mongodb.net:27017,cluster0-shard-00-02.ganlz.mongodb.net:27017/igclone?ssl=true&replicaSet=atlas-ixzboz-shard-0&authSource=admin&retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key',
+};
